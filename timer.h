90dd4a8a74bc0a8762da4691182388ee09d5cd7e
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2023-10-24 17:43:16 +0200

    feat(timer) : add timer files from moodle
    Update cmake aussi

---
+++ timer.h
@@ -0,0 +1,44 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+
+// timer structure
+typedef struct s_timer
+{
+    clock_t _start; // start time
+    clock_t _end;   // end time
+    double _msecs;  // duration in milliseconds
+} t_timer;
+
+static t_timer _timer;
+
+/**
+ * @brief start the timer
+ * @param none
+ * @return none
+ */
+void startTimer();
+
+/**
+ * @brief stop the timer
+ * @param none
+ * @return none
+ */
+void stopTimer();
+
+/**
+ * @brief display the time
+ * @param none
+ * @return none
+ */
+void displayTime();
+
+/**
+ * @brief return a string with the time in seconds and milliseconds
+ * @param none
+ * @return a string with the time in seconds and milliseconds
+ */
+char *getTimeAsString();
+
+#endif //TIMER_H
