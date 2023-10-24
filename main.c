4120fe8fc4505157f8867fbf112e384674335bda
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2023-10-24 16:44:17 +0200

    Init main + gitignore + CMAKE
    Lire l'exemple de documentation pour l'appliquer au reste du projet !

---
+++ main.c
@@ -0,0 +1,22 @@
+/**
+ * @file main.c
+ * @author Mathurin Lemoine / Thomas Fischer / Idir Nait Meddour
+ * @brief Fichier contenant la fonction main
+ * @version 1.0
+ * @date 24/10/2023
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    printf("Hello world !\n");
+    return 0;
+}
+
+/**
+ * @fn int main()
+ * @brief Fonction principale du programme
+ * @return La valeur de sortie du programme
+ */
