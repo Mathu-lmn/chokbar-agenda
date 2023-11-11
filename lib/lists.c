f021218fd4308ecb22398a79c160bc5a239081c5
Thomas Fischer <thomas.fischer67@outlook.fr>
2023-11-11 18:29:31 +0100

    nom de variable
    changement de n en max_level_power

--- lib/lists.c
+++ lib/lists.c
@@ -110,13 +110,13 @@ void print_all_levels(t_list list) {
 /**
  * @brief Fonction de création d'une skiplist "dichotomique"
  * @param list La skiplist
- * @param n Nombre qui définit le nombre de cellule au niveau 0 par la formule 2^n - 1
+ * @param max_level_power Nombre qui définit le nombre de cellule au niveau 0 par la formule 2^n - 1
  * @return un pointeur vers la skiplist créée
  */
-t_list * create_better_list(int n){
-    t_list* my_list = create_list(n);
+t_list * create_better_list(int max_level_power){
+    t_list* my_list = create_list(max_level_power);
 
-    int nb_cell = pow(2, n)-1;
+    int nb_cell = pow(2, max_level_power)-1;
     int * levels = malloc(nb_cell*sizeof(int));
     if (levels == NULL){
         printf("Erreur d'allocation mémoire");
