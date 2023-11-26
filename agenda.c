9de1e4484a8090346dce503f1791445bf9f22645
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2023-11-26 20:46:54 +0100

    Begin menu functions

--- agenda.c
+++ agenda.c
@@ -120,3 +120,108 @@ void add_contact_to_agenda(t_agenda *agenda, t_agenda_cell *agenda_entry) {
         }
     }
 }
+
+// Rechercher un contact, et proposer une complétion automatique à partir de la 3ème lettre entrée pour le nom (il faudra donc faire la saisie du nom de recherche caractère par caractère) ;
+void startSearch() {
+
+}
+
+t_agenda_cell * search_contact(t_agenda * agenda, char * nom, char * prenom) {
+    if (agenda == NULL) {
+        printf("Agenda non initialisé.\n");
+        return NULL;
+    }
+
+    t_agenda_cell * curr = agenda->heads[3];
+    int i = 3;
+    while (curr != NULL) {
+        if (strcmp(curr->contact.nom, nom) == 0 && strcmp(curr->contact.prenom, prenom) == 0) {
+            return curr;
+        }
+        // si on a dépassé le nom recherché, on arrête la recherche
+        if (strcmp(curr->contact.nom, nom) > 0) {
+            return NULL;
+        }
+        if (curr->tab_next[i] == NULL || strcmp(curr->tab_next[i]->contact.nom, nom) > 0) {
+            i--;
+        } else {
+            curr = curr->tab_next[i];
+        }
+    }
+    return NULL;
+}
+
+void displayContactRdv(t_agenda *agenda) {
+    printf("Nom du contact: ");
+    char *nom = scanString();
+    printf("Prénom du contact: ");
+    char *prenom = scanString();
+
+    t_agenda_cell * contact = search_contact(agenda, nom, prenom);
+    if (contact == NULL) {
+        printf("Contact non trouvé.\n");
+        return;
+    }
+    if (contact->rdv == NULL) {
+        printf("Aucun rendez-vous pour ce contact.\n");
+        return;
+    }
+    printf("Rendez-vous de %s %s:\n", contact->contact.prenom, contact->contact.nom);
+    t_rdv *rdv = contact->rdv;
+    while (rdv != NULL) {
+        printf("Date: %d/%d/%d\t", rdv->date.jour, rdv->date.mois, rdv->date.annee);
+        printf("Heure: %d:%d\t", rdv->heure.heure, rdv->heure.minute);
+        printf("Durée: %d:%d\t", rdv->duree.heure, rdv->duree.minute);
+        printf("Objet: %s\n", rdv->objet);
+        rdv = rdv->suivant;
+    }
+}
+
+int executeChoice(int choice, t_agenda * agenda) {
+    // Traitement de l'option choisie
+    switch (choice) {
+        case 1:
+            startSearch();
+
+        case 2:
+            displayContactRdv(agenda);
+
+        case 3:
+            // Créer un contact
+            // ...
+            break;
+
+        case 4:
+            // Créer un rendez-vous pour un contact
+            // ...
+            break;
+
+        case 5:
+            // Supprimer un rendez-vous
+            // ...
+            break;
+
+        case 6:
+            // Sauvegarder le fichier de tous les rendez-vous
+            // ...
+            break;
+
+        case 7:
+            // Charger un fichier de rendez-vous
+            // ...
+            break;
+
+        case 8:
+            // Fournir les temps de calcul pour une insertion de nouveau contact
+            // ...
+            break;
+
+        case 0:
+            return 0;
+
+        default:
+            printf("Option invalide. Veuillez réessayer.\n");
+            break;
+    }
+    return 1;
+}
