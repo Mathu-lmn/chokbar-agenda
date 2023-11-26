aa1170401cbd09797a76e4322c19e0fd4ebd6602
Thomas Fischer <thomas.fischer67@outlook.fr>
2023-11-26 18:53:22 +0100

    fin partieII
    pour obtenir un fichier texte avec les temps de recherches

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include "lib/lists.h"
 #include "lib/cells.h"
+#include "lib/timer.h"
 
 /**
  * @brief Fonction principale du programme
@@ -33,24 +34,50 @@ int main() {
     print_all_levels(*my_list);
     printf("\n");
 
-    t_list * simpleList = create_better_list(3);
-    print_all_levels(*simpleList);
-    printf("\n");
-    better_search(*simpleList, -2);
-    printf("\n");
 
+    FILE *temps = fopen("temps.txt","w");
+    char format[] = "%d\t%s\t%s\n" ;
+    char *time_lvl0;
+    char *time_all_levels;
 
-    t_list * bigList = create_better_list(14);
-    classic_search(*bigList, 5842);
-    printf("\n");
-    better_search(*bigList, 5842);
+    int level = 7;
+    printf("Veuillez choisir un nombre de niveau a tester > 7 : ");
+    scanf("%d", &level);
+    while(level <= 7){
+        printf("nombre de niveau a tester < 7. Choisissez un autre nombre : ");
+        scanf("%d", &level);
+    }
+
+    for (int i = 7; i < level; i++) {
+        time_t t1;
+        srand(time(&t1));
+        int nb_cell = (int) (pow(2, i) + 1e-9)-1;
+        int number;
+        t_list *new_list = create_better_list(i);
+        startTimer();
+        for (int j = 0; j < 10000; j++) {
+            number = rand() % (nb_cell);
+            classic_search(*new_list, number);
+        }
+        stopTimer();
+        time_lvl0 = getTimeAsString();
+
+        startTimer();
+        for (int j = 0; j < 10000; j++) {
+            number = rand() %(nb_cell);
+            better_search(*new_list, number);
+        }
+        stopTimer();
+        time_all_levels = getTimeAsString();
+        fprintf(temps,format,i,time_lvl0, time_all_levels);
+        free (new_list);
+        new_list = NULL;
+    }
+    fclose(temps);
+
 
     free(my_list);
-    free(simpleList);
-    free(bigList);
     my_list = NULL;
-    simpleList = NULL;
-    bigList = NULL;
 
     return 0;
 }
