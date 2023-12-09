cb799878cfd315fcc668681fe573ff9b06b749e1
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2023-12-09 14:07:20 +0100

    Only partie 1

--- main.c
+++ main.c
@@ -33,65 +33,6 @@ int main() {
     insert_cell(my_list, 2, 15);
     print_all_levels(*my_list);
     printf("\n");
-
-
-    FILE *temps;
-    if ((temps = fopen("temps.txt", "w")) == NULL) {
-        printf("Erreur lors de l'ouverture du fichier temps.\n");
-        exit(EXIT_FAILURE);
-    }
-    char format[] = "%d\t%s\t%s\n" ;
-
-    int level = 7;
-    printf("Veuillez choisir un nombre de niveau a tester entre 8 et 16 : ");
-    scanf("%d", &level);
-    while(level < 8 || level > 16){
-        printf("Nombre de niveaux invalide. Choisissez un autre nombre : ");
-        scanf("%d", &level);
-    }
-
-    for (int i = 7; i < level; i++) {
-        char *time_lvl0;
-        char *time_all_levels;
-        time_t t1;
-        srand(time(&t1));
-        int nb_cell = (int) (pow(2, i) + 1e-9)-1;
-        int number;
-        t_list *new_list = create_better_list(i);
-        startTimer();
-        for (int j = 0; j < 10000; j++) {
-            number = rand() % (nb_cell);
-            classic_search(*new_list, number);
-        }
-        stopTimer();
-        time_lvl0 = getTimeAsString();
-
-        startTimer();
-        for (int j = 0; j < 10000; j++) {
-            number = rand() %(nb_cell);
-            better_search(*new_list, number);
-        }
-        stopTimer();
-        time_all_levels = getTimeAsString();
-        fprintf(temps,format,i,time_lvl0, time_all_levels);
-        for(int z = 0; z<i; z++){
-            free(new_list->heads[z]);
-            new_list->heads[z] = NULL;
-        }
-        free(new_list->heads);
-        new_list->heads = NULL;
-        free (new_list);
-        new_list = NULL;
-        free(time_lvl0);
-        free(time_all_levels);
-        time_lvl0 = NULL;
-        time_all_levels = NULL;
-    }
-    fclose(temps);
-
-    free(my_list);
-    my_list = NULL;
-
     return 0;
 }
 
