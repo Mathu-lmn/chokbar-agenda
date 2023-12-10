a5581b8387c0e1f108b8f3c45bd58668331984d6
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2023-12-10 22:54:49 +0100

    Final comments for partie 1

--- lib/lists.c
+++ lib/lists.c
@@ -9,11 +9,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * @brief Fonction de création d'une skiplist
- * @param max_levels Le nombre maximum de niveaux de la skiplist
- * @return Un pointeur vers la skiplist créée
- */
 t_list * create_list(int max_levels) {
     t_list * new_list = malloc(sizeof(t_list));
     new_list->max_levels = max_levels;
@@ -26,12 +21,6 @@ t_list * create_list(int max_levels) {
     return new_list;
 }
 
-/**
- * @brief Fonction d'insertion d'une cellule dans une skiplist
- * @param list Un pointeur vers la skiplist
- * @param levels Le niveau de la cellule à insérer
- * @param value La valeur de la cellule à insérer
- */
 void insert_cell(t_list* list, int levels, int value) {
     if (levels < 1 || levels > list->max_levels) {
         printf("Invalid level.\n");
@@ -61,11 +50,6 @@ void insert_cell(t_list* list, int levels, int value) {
     }
 }
 
-/**
- * @brief Fonction d'affichage d'un niveau d'une skiplist
- * @param list La skiplist
- * @param level Le niveau à afficher
- */
 void print_level(t_list list, int level) {
     if (level < 0 || level >= list.max_levels) {
         printf("Valeur de level invalide\n");
@@ -81,10 +65,6 @@ void print_level(t_list list, int level) {
     printf("NULL\n");
 }
 
-/**
- * @brief Fonction d'affichage de tous les niveaux d'une skiplist
- * @param list La skiplist
- */
 void print_all_levels(t_list list) {
     print_level(list, 0);
 
@@ -105,11 +85,6 @@ void print_all_levels(t_list list) {
     }
 }
 
-/**
- * @brief Fonction de création d'une skiplist "dichotomique"
- * @param max_level_power Nombre qui définit le nombre de cellule au niveau 0 par la formule 2^n - 1
- * @return un pointeur vers la skiplist créée
- */
 t_list * create_better_list(int max_level_power){
     t_list* my_list = create_list(max_level_power);
 
@@ -137,11 +112,6 @@ t_list * create_better_list(int max_level_power){
 }
 
 
-/**
- * @brief Fonction de recherche d'une valeur dans le niveau zéro d'une skiplist
- * @param list La skiplist
- * @param val La valeur à trouver
- */
 void classic_search (t_list list, int val){
     t_cell * current = list.heads[0];
     while (current != NULL){
@@ -154,28 +124,26 @@ void classic_search (t_list list, int val){
     //printf("%d not found", val);
 }
 
-/**
- * @brief Fonction de recherche d'une valeur à partir du plus haut niveau d'une skiplist
- * @param list La skiplist
- * @param val La valeur à trouver
- */
 void better_search (t_list list, int val){
-    int current_level = list.max_levels - 1;
-    t_cell * current = list.heads[current_level];
-
-    while ((current != NULL) && (current_level >= 0)){
+    t_cell * current = list.heads[list.max_levels - 1];
+    while (current != NULL){
         if (current->value == val){
+            //printf ("%d found", val);
             return;
         }
         if (current->value < val){
-            if ((current->next[current_level] == NULL || current->next[current_level]->value > val)) {
-                current_level--;
+            if ((current->next[current->level] == NULL || current->next[current->level]->value > val)
+                && current->level != 0){
+                current->level = current->level - 1;
             } else {
-                current = current->next[current_level];
+                current = current->next[current->level];
             }
+        } else if (current->level - 1 >= 0){
+            --current->level;
+            current = list.heads[current->level];
         } else {
-            current_level--;
-            if (current_level >= 0) current = list.heads[current_level];
+            //printf("%d not found", val);
+            break;
         }
     }
 }
